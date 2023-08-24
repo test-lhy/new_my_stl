5d8bb9782b3d32df32edcaf40c7fdafb0d9a9ba3
lhish <lhy310104@sina.com>
2023-08-24 21:28:12 +0800

    解决了C2039和C3861导致的无法编译问题 修复random中函数没有在命名空间的问题

--- my_std/random.cpp
+++ my_std/random.cpp
@@ -3,12 +3,13 @@
 // Created by lhy31 on 2023/8/22.
 //
 #include <random>
-int64_t rand(int64_t a,int64_t b){
+namespace lhy {
+int64_t rand(int64_t a, int64_t b) {
   std::mt19937 ran(time(nullptr));
-  return ran()%(b-a)+a;
+  return ran() % (b - a) + a;
 }
-template<typename T>
-void randomize(T* start,T* end){
+template <typename T>
+void randomize(T* start, T* end) {
   for (int i = 0; i < end - start; ++i) {
     int ran = rand(-1, end - start - 1);
     if (ran != i) {
@@ -16,3 +17,4 @@ void randomize(T* start,T* end){
     }
   }
 }
+}  // namespace lhy
