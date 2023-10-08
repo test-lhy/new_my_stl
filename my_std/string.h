53d6f4e127d856346ffc13cdcdd1801d61e2e083
lhish <lhy310104@sina.com>
2023-10-08 12:10:30 +0800

    为vector增加了一些运算符，并且完成了流运算符，完成了string，但是未完成测试

---
+++ my_std/string.h
@@ -0,0 +1,10 @@
+//
+// Created by lhy on 23-10-8.
+//
+#include "vector.h"
+#ifndef MY_STL_STRING_H
+#define MY_STL_STRING_H
+namespace lhy{
+using string=vector<char>;
+}
+#endif  // MY_STL_STRING_H
