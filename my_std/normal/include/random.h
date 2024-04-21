c207aeb8faa41a9efdcd987aa81f302794648391
lhish <lhy310104@sina.com>
2024-04-21 16:27:56 +0800

    [fix] 修复了randomize的问题

--- my_std/normal/include/random.h
+++ my_std/normal/include/random.h
@@ -6,13 +6,14 @@
 #define MY_STL_RANDOM_H
 #include <cstdint>
 #include <random>
+
 #include "data_structure.h"
 namespace lhy {
-[[nodiscard]]int64_t rand(int64_t, int64_t);
+[[nodiscard]] int64_t rand(int64_t, int64_t);
 template <typename T>
 void randomize(NormIterator<T> start, NormIterator<T> end) {
   for (int i = 0; i < end - start; ++i) {
-    int ran = rand(0, end - start - 1);
+    int ran = rand(0, end - start);
     if (ran != i) {
       std::swap(start[i], start[ran]);
     }
