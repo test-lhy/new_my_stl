6acf7c16c3dc81ecd5c7b01f0d2dec9f1d53be5c
lhish <lhy310104@sina.com>
2025-02-26 18:32:54 +0800

    [feat,fix] 修复了只对某一个类有效的bug 将成员相关的所有函数都适配静态成员

--- my_std/normal/include/refl_s.h
+++ my_std/normal/include/refl_s.h
@@ -30,6 +30,15 @@ constexpr int GetMemberIndex(const std::string& name) {
   return refl_s<T>::GetMemberIndex(name);
 }
 template <typename T>
+constexpr int GetMemberIsStatic(const std::string& name) {
+  return refl_s<T>::GetMemberIsStatic(name);
+}
+
+template <typename T, typename U>
+constexpr T& GetStaticMember(const std::string& name) {
+  return std::any_cast<std::reference_wrapper<T>>(refl_s<U>::GetStaticMember(name)).get();
+}
+template <typename T>
 constexpr AccessSpecifier GetMemberAccessSpecifier(const std::string& name) {
   return refl_s<T>::GetMemberAccessSpecifier(name);
 }
@@ -42,6 +51,7 @@ class AccessSpecifierHelper {};
 template <typename>
 class AccessSpecifierGetter {};
 }  // namespace lhy
+// static对象必须是已经定义的了,不管是类内inline还是类外define
 #define CONCEPT_OF_KID_ACCESS(Class, KidName) \
   template <typename T>                       \
   concept kid_access_##Class##_##KidName = requires() { T::KidName; };
@@ -70,7 +80,7 @@ class AccessSpecifierGetter {};
   };
 #define CREATE_SPECIFIC_ACCESS_SPECIFIER(Class, KidName)                                       \
   static constexpr auto access_specifier_##KidName =                                           \
-      lhy::detail::kid_access_base_##KidName<Class>                                            \
+      lhy::detail::kid_access_##Class##_##KidName<Class>                                       \
           ? AccessSpecifier::PUBLIC                                                            \
           : (AccessSpecifierHelper<Class>::is_protected_##KidName ? AccessSpecifier::PROTECTED \
                                                                   : AccessSpecifier::PRIVATE);
@@ -80,25 +90,44 @@ class AccessSpecifierGetter {};
    public:                                                         \
     FOR2EACH(CREATE_SPECIFIC_ACCESS_SPECIFIER, Class, __VA_ARGS__) \
   };
-#define CREATE_ROBBER1(Class, KidName)           \
-  template <auto T>                              \
-  class Class##KidName##RetType {};              \
-  constexpr auto GetPtr##Class##KidName##Type(); \
-  template <typename T
-#define CREATE_ROBBER2(Class, KidName)                                                                   \
-  T Class::* M > class Class##KidName##RetType<M> {                                                      \
-   public:                                                                                               \
-    using RealType = T;                                                                                  \
-    friend constexpr auto GetPtr##Class##KidName##Type() { return RealType{}; };                         \
-  };                                                                                                     \
-  template class Class##KidName##RetType<&Class::KidName>;                                               \
-  using Ret##Class##KidName##Type = FunctionProperties<decltype(GetPtr##Class##KidName##Type)>::RetType; \
-  constexpr Ret##Class##KidName##Type Class::* GetPtr##Class##KidName();                                 \
-  template <Ret##Class##KidName##Type Class::* M>                                                        \
-  struct Rob##Class##KidName {                                                                           \
-    friend constexpr Ret##Class##KidName##Type Class::* GetPtr##Class##KidName() { return M; }           \
-  };                                                                                                     \
+#define CREATE_ROBBER_ANOTHER1(Class, KidName) template <typename T
+#define CREATE_ROBBER_ANOTHER2(Class, KidName)                                                                         \
+  T* M > class Class##KidName##RetType<M> {                                                                            \
+   public:                                                                                                             \
+    using RealType = T;                                                                                                \
+    using PointerType = T*;                                                                                            \
+    friend constexpr auto GetPtr##Class##KidName##Type() { return RealType{}; };                                       \
+    friend constexpr auto GetPtr##Class##KidName##PointerType() { return PointerType{}; };                             \
+    constexpr static bool is_static = true;                                                                            \
+    friend constexpr auto Get##Class##KidName##IsStatic() { return is_static; };                                       \
+  };                                                                                                                   \
+  template class Class##KidName##RetType<&Class::KidName>;                                                             \
+  using Ret##Class##KidName##Type = FunctionProperties<decltype(GetPtr##Class##KidName##Type)>::RetType;               \
+  using Ret##Class##KidName##PointerType = FunctionProperties<decltype(GetPtr##Class##KidName##PointerType)>::RetType; \
+  constexpr bool Class##KidName##_is_static = Get##Class##KidName##IsStatic();                                         \
+  constexpr Ret##Class##KidName##PointerType GetPtr##Class##KidName();                                                 \
+  template <Ret##Class##KidName##PointerType M>                                                                        \
+  struct Rob##Class##KidName {                                                                                         \
+    friend constexpr Ret##Class##KidName##PointerType GetPtr##Class##KidName() { return M; }                           \
+  };                                                                                                                   \
   template struct Rob##Class##KidName<&Class::KidName>;
+#define CREATE_ROBBER1(Class, KidName)                  \
+  template <auto T>                                     \
+  class Class##KidName##RetType {};                     \
+  constexpr auto GetPtr##Class##KidName##Type();        \
+  constexpr auto GetPtr##Class##KidName##PointerType(); \
+  constexpr auto Get##Class##KidName##IsStatic();       \
+  template <typename T
+#define CREATE_ROBBER2(Class, KidName)                                                     \
+  T Class::* M > class Class##KidName##RetType<M> {                                        \
+   public:                                                                                 \
+    using RealType = T;                                                                    \
+    using PointerType = T Class::*;                                                        \
+    friend constexpr auto GetPtr##Class##KidName##Type() { return RealType{}; };           \
+    friend constexpr auto GetPtr##Class##KidName##PointerType() { return PointerType{}; }; \
+    constexpr static bool is_static = false;                                               \
+    friend constexpr auto Get##Class##KidName##IsStatic() { return is_static; };           \
+  };
 #define GET_KID(Class, KidName) GetPtr##Class##KidName()
 #define GET_KID_TYPE(Class, KidName) decltype(GET_KID(Class, KidName))
 #define GET_KID_NAME_AND_NUM1(KidName, num) \
@@ -107,6 +136,7 @@ class AccessSpecifierGetter {};
 #define GET_KID_NAME_AND_NUM2(KidName, num) \
   num                                       \
   }
+#define GET_KID_STATIC(Class, KidName) Class##KidName##_is_static
 #define GET_KID_ACCESS_SPECIFIER(Class, KidName) AccessSpecifierGetter<Class>::access_specifier_##KidName
 #define REFL(Class, ...)                                                                                               \
   namespace lhy {                                                                                                      \
@@ -117,6 +147,7 @@ class AccessSpecifierGetter {};
   CREATE_ACCESS_SPECIFIER_GETTER(Class, __VA_ARGS__)                                                                   \
   CREATE_ACCESS_SPECIFIER(Class, __VA_ARGS__)                                                                          \
   FOR22EACH(CREATE_ROBBER1, CREATE_ROBBER2, Class, __VA_ARGS__)                                                        \
+  FOR22EACH(CREATE_ROBBER_ANOTHER1, CREATE_ROBBER_ANOTHER2, Class, __VA_ARGS__)                                        \
   template <>                                                                                                          \
   class refl_s<Class> {                                                                                                \
    public:                                                                                                             \
@@ -124,8 +155,46 @@ class AccessSpecifierGetter {};
         FOR_COMMA2_EACH(GET_KID, Class, __VA_ARGS__)};                                                                 \
     constexpr static std::array<std::pair<std::string, int>, COUNT_ARGS(__VA_ARGS__)> member_names{                    \
         {FOR_COMMA22num_EACH(GET_KID_NAME_AND_NUM1, GET_KID_NAME_AND_NUM2, __VA_ARGS__)}};                             \
+    constexpr static std::array<bool, COUNT_ARGS(__VA_ARGS__)> member_static{                                          \
+        FOR_COMMA2_EACH(GET_KID_STATIC, Class, __VA_ARGS__)};                                                          \
     constexpr static std::array<AccessSpecifier, COUNT_ARGS(__VA_ARGS__)> member_access_specifier{                     \
         FOR_COMMA2_EACH(GET_KID_ACCESS_SPECIFIER, Class, __VA_ARGS__)};                                                \
+    template <int Index>                                                                                               \
+    constexpr static decltype(auto) GetStaticMemberReal() {                                                            \
+      if constexpr (std::get<Index>(member_static)) {                                                                  \
+        return *std::get<Index>(members);                                                                              \
+      } else {                                                                                                         \
+        throw std::runtime_error("member isn't static");                                                               \
+        static Class a;                                                                                                \
+        return a.*std::get<Index>(members);                                                                            \
+      }                                                                                                                \
+    }                                                                                                                  \
+    template <int Index>                                                                                               \
+    constexpr static decltype(auto) GetMemberReal(Class& object) {                                                     \
+      if constexpr (std::get<Index>(member_static)) {                                                                  \
+        return *std::get<Index>(members);                                                                              \
+      } else {                                                                                                         \
+        return object.*std::get<Index>(members);                                                                       \
+      }                                                                                                                \
+    }                                                                                                                  \
+    template <int Index>                                                                                               \
+    constexpr static decltype(auto) GetMemberReal(const Class& object) {                                               \
+      if constexpr (std::get<Index>(member_static)) {                                                                  \
+        return *std::get<Index>(members);                                                                              \
+      } else {                                                                                                         \
+        return object.*std::get<Index>(members);                                                                       \
+      }                                                                                                                \
+    }                                                                                                                  \
+    constexpr static bool GetMemberIsStatic(const std::string& name) {                                                 \
+      const int member_index = GetMemberIndex(name);                                                                   \
+      return *static_for_with_return<0, MINUS(COUNT_ARGS(__VA_ARGS__))(), IndexAddOne, std::optional<bool>>(           \
+          [&member_index]<int I>() -> std::optional<bool> {                                                            \
+            if (I == member_index) {                                                                                   \
+              return std::get<I>(member_static);                                                                       \
+            }                                                                                                          \
+            return {};                                                                                                 \
+          });                                                                                                          \
+    }                                                                                                                  \
     constexpr static int GetMemberIndex(const std::string& name) {                                                     \
       const std::array<std::pair<std::string, int>, COUNT_ARGS(__VA_ARGS__)>::const_iterator result =                  \
           std::ranges::find_if(member_names,                                                                           \
@@ -135,13 +204,27 @@ class AccessSpecifierGetter {};
       }                                                                                                                \
       return result->second;                                                                                           \
     }                                                                                                                  \
+    constexpr static std::any GetStaticMember(const std::string& name) {                                               \
+      const int member_index = GetMemberIndex(name);                                                                   \
+      if (member_static[member_index] == 0) {                                                                          \
+        throw std::runtime_error("member isn't static");                                                               \
+      }                                                                                                                \
+      return static_for_with_return<0, MINUS(COUNT_ARGS(__VA_ARGS__))(), IndexAddOne, std::any>(                       \
+          [&member_index]<int I>() -> std::any {                                                                       \
+            if (I == member_index) {                                                                                   \
+              return std::reference_wrapper<std::decay_t<decltype(GetStaticMemberReal<I>())>>(                         \
+                  GetStaticMemberReal<I>());                                                                           \
+            }                                                                                                          \
+            return {};                                                                                                 \
+          });                                                                                                          \
+    }                                                                                                                  \
     constexpr static std::any GetMember(Class& object, const std::string& name) {                                      \
       const int member_index = GetMemberIndex(name);                                                                   \
       return static_for_with_return<0, MINUS(COUNT_ARGS(__VA_ARGS__))(), IndexAddOne, std::any>(                       \
           [&member_index, &object]<int I>() -> std::any {                                                              \
             if (I == member_index) {                                                                                   \
-              return std::reference_wrapper<std::decay_t<decltype(object.*std::get<I>(members))>>(                     \
-                  object.*std::get<I>(members));                                                                       \
+              return std::reference_wrapper<std::decay_t<decltype(GetMemberReal<I>(object))>>(                         \
+                  GetMemberReal<I>(object));                                                                           \
             }                                                                                                          \
             return {};                                                                                                 \
           });                                                                                                          \
@@ -151,8 +234,8 @@ class AccessSpecifierGetter {};
       return static_for_with_return<0, MINUS(COUNT_ARGS(__VA_ARGS__))(), IndexAddOne, std::any>(                       \
           [&member_index, &object]<int I>() -> std::any {                                                              \
             if (I == member_index) {                                                                                   \
-              return std::reference_wrapper<std::add_const_t<std::decay_t<decltype(object.*std::get<I>(members))>>>(   \
-                  object.*std::get<I>(members));                                                                       \
+              return std::reference_wrapper<std::add_const_t<std::decay_t<decltype(GetMemberReal<I>(object))>>>(       \
+                  GetMemberReal<I>(object));                                                                           \
             }                                                                                                          \
             return {};                                                                                                 \
           });                                                                                                          \
--- refl_s_test.cpp
+++ refl_s_test.cpp
@@ -25,62 +25,37 @@ class base {
 };
 REFL(base, a1, a2, a3, a4)
 
-// class node2 {
-//   int a;
-//   int b, c;
-//   float d;
-//   char e;
-//   double f;
-//   bool g;
-//
-//  public:
-//   node2() {};
-//   node2(int a, int b, int c, float d, char e, double f, bool g) : a(a), b(b), c(c), d(d), e(e), f(f), g(g) {}
-//   ~node2() = default;
-//   node2(const node2& other) = default;
-//   node2(node2&& other) = default;
-//   node2& operator=(const node2& other) = default;
-//   node2& operator=(node2&& other) = default;
-//   void h();
-//   static int m;
-//   static int m1() {};
-//
-//  protected:
-//   void i();
-//   int j;
-//
-//  private:
-//   int k;
-//   void l();
-// };
-// struct node : base {
-//   using type_now = int;
-//   int a;
-//   int b, c;
-//   float d;
-//   char e;
-//   double f;
-//   bool g;
-//   node() = default;
-//   node(int a, int b, int c, float d, char e, double f, bool g) : a(a), b(b), c(c), d(d), e(e), f(f), g(g) {}
-//   ~node() = default;
-//   node(const node& other) = default;
-//   node(node&& other) = default;
-//   node& operator=(const node& other) = default;
-//   node& operator=(node&& other) = default;
-//   void h();
-//   node2 n;
-//   static int m;
-//   static int m1() {};
-//
-//  protected:
-//   void i();
-//   int j;
-//
-//  private:
-//   int k;
-//   void l();
-// };
+class node2 {
+ public:
+  int a;
+  int b, c;
+  float d;
+  char e;
+  double f;
+  bool g;
+
+  node2() {};
+  node2(int a, int b, int c, float d, char e, double f, bool g) : a(a), b(b), c(c), d(d), e(e), f(f), g(g) {}
+  ~node2() = default;
+  node2(const node2& other) = default;
+  node2(node2&& other) = default;
+  node2& operator=(const node2& other) = default;
+  node2& operator=(node2&& other) = default;
+  void h();
+  inline static int m{114514};
+  static int m1() { return 1; };
+
+ protected:
+  inline static int m2{114514};
+  void i();
+  int j;
+
+ private:
+  inline static int m3{114514};
+  int k;
+  void l();
+};
+REFL(node2, m, m2, m3)
 
 void test1() {
   auto t = refl_s<base>::members;
@@ -110,9 +85,36 @@ void test3() {
   assert(GetMemberAccessSpecifierInString<base>("a3") == "protected");
   assert(GetMemberAccessSpecifierInString<base>("a4") == "private");
 }
+void test4() {
+  node2 a;
+  auto& c = GetMember<int>(a, "m3");
+  auto& c1 = GetStaticMember<int, node2>("m3");
+  std::cout << c << std::endl;
+  std::cout << c1 << std::endl;
+  assert(c == c1);
+  c = 5;
+  assert(c == c1);
+  assert(c == 5);
+  c = 10;
+  assert(c == c1);
+  assert(c == 10);
+  const node2 a1{};
+  auto& c2 = GetMember<int>(a1, "m3");
+  static_assert(std::is_same_v<decltype(c2), const int&>);
+}
+void test5() {
+  std::cout << GetMemberAccessSpecifierInString<node2>("m") << std::endl;
+  std::cout << GetMemberAccessSpecifierInString<node2>("m2") << std::endl;
+  std::cout << GetMemberAccessSpecifierInString<node2>("m3") << std::endl;
+  assert(GetMemberAccessSpecifierInString<node2>("m") == "public");
+  assert(GetMemberAccessSpecifierInString<node2>("m2") == "protected");
+  assert(GetMemberAccessSpecifierInString<node2>("m3") == "private");
+}
 int main() {
   test1();
   test2();
   test3();
+  test4();
+  test5();
   std::cout << "all test passed";
 }
